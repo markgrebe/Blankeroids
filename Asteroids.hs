4a8b83c739a185b0f9c02e68ecdc54f49ee7fb65
Mark Grebe <mark.grebe@ku.edu>
2014-12-01 20:19:05 -0600

    Finished Game Over

--- Asteroids.hs
+++ Asteroids.hs
@@ -18,7 +18,7 @@ import Control.Monad.Random
 
 import IdentityList
 
-import Debug.Trace
+-- import Debug.Trace
 -- import Debug.Hood.Observe
 
 ---------------------------------------------------
@@ -403,7 +403,7 @@ movingShip g s = proc ev -> do
     newMissile :: Position -> AngPosition -> Object
     newMissile p' ap' = Missile { poly = missilePolygon,
                                   pos = vector2 ((vector2X p') - 0.016 * sin ap')
-                                                ((vector2Y p') + 0.016 * cos ap'), -- ToDo: caculate ship tip pos
+                                                ((vector2Y p') + 0.016 * cos ap'),
                                   vel = vector2 (-missileVel * sin ap')
                                                 ( missileVel * cos ap'),
                                   done = NoEvent, spawn  = NoEvent }
@@ -496,11 +496,11 @@ playingGame gm = proc ev -> do
   where
     accumScore :: Int -> GameEvent -> Int
     accumScore start GameChange { scoreChanged = s} = start + s
-    accumScore start _         = start
+    accumScore start _                              = start
 
     accumLives :: Int -> GameEvent -> Int
     accumLives start GameChange {shipDestroyed = sd} | sd == True = start - 1
-    accumLives start _             = start
+    accumLives start _                                            = start
 
     accumRounds :: Int -> GameEvent -> Int
     accumRounds start GameChange {newRound = rc} | rc == True = start + 1
@@ -742,7 +742,8 @@ renderShip s = do
     if (thrusting s) && not (null (poly s)) then do
         renderPolygon thrustPolygon
         stroke()
-    else lineWidth 0.002
+    else
+        return()
     restore ()
 
 renderAsteroid :: Object -> Canvas ()
@@ -784,22 +785,28 @@ renderGame g = do
     strokeStyle "white"
     stroke()
     if gameOver g then do
-       renderPolygon junkPolygon
-       stroke()
+        renderPolygons placedGameOverPolygons
+        stroke()
     else
-       lineWidth 0.002
+        return()
     restore ()
   where
     placedLifePolygons :: [Polygon]
     placedLifePolygons = map translatePolyPair (zip shipPositions lifePolygons)
     placedScorePolygons :: [Polygon]
     placedScorePolygons = map translatePolyPair (zip digitPositions scorePolygons)
+    placedGameOverPolygons :: [Polygon]
+    placedGameOverPolygons = map translatePolyPair (zip gameOverPositions gameOverPolygons)
     translatePolyPair :: (Point, Polygon) -> Polygon
     translatePolyPair (pt,pol) = translatePoly pt pol
     lifePolygons :: [Polygon]
     lifePolygons = take (lives g) (repeat shipPolygon)
     scorePolygons :: [Polygon]
     scorePolygons = map digToPolygon (reverse $ digs (score g))
+    gameOverPolygons :: [Polygon]
+    gameOverPolygons = [letterGPolygon, letterAPolygon, letterMPolygon,
+                        letterEPolygon, letterSpacePolygon, letterOPolygon,
+                        letterVPolygon, letterEPolygon, letterRPolygon]
     digToPolygon :: Int -> Polygon
     digToPolygon d = digitPolygons !! d
     digs :: Int -> [Int]
@@ -808,6 +815,8 @@ renderGame g = do
     digitPositions = zip [0.220,0.180..0.020] (take 6 $ repeat 0.950)
     shipPositions :: [Point]
     shipPositions =  zip [0.220,0.200..0.120] (take 6 $ repeat 0.890)
+    gameOverPositions :: [Point]
+    gameOverPositions = zip [0.340,0.380..0.700] (take 9 $ repeat 0.5)
 
 renderObject :: Object -> Canvas ()
 renderObject obj = case obj of
@@ -916,3 +925,37 @@ digitPolygons = [digitZeroPolygon, digitOnePolygon, digitTwoPolygon,
                   digitThreePolygon, digitFourPolygon, digitFivePolygon,
                   digitSixPolygon, digitSevenPolygon, digitEightPolygon,
                   digitNinePolygon]
+
+
+letterAPolygon :: Polygon
+letterAPolygon  = [(-0.015,-0.023),(-0.015, 0.008),( 0.000, 0.023),
+                   ( 0.015, 0.008),( 0.015,-0.023),( 0.015,-0.008),
+                   (-0.015,-0.008)]
+
+letterEPolygon :: Polygon
+letterEPolygon  = [( 0.015,-0.023),(-0.015,-0.023),(-0.015, 0.023),
+                   ( 0.015, 0.023),(-0.015, 0.023),(-0.015, 0.000),
+                   ( 0.008, 0.000)]
+
+letterGPolygon :: Polygon
+letterGPolygon  = [( 0.000,-0.008),( 0.015,-0.008),( 0.015,-0.023),
+                   (-0.015,-0.023),(-0.015, 0.023),( 0.015, 0.023),
+                   ( 0.015, 0.008)]
+
+letterMPolygon :: Polygon
+letterMPolygon  = [(-0.015,-0.023),(-0.015, 0.023),(0.000,0.008),
+                   ( 0.015, 0.023),( 0.015,-0.023)]
+
+letterOPolygon :: Polygon
+letterOPolygon  = digitZeroPolygon
+
+letterRPolygon :: Polygon
+letterRPolygon = [(-0.015,-0.023),(-0.015, 0.023),( 0.015, 0.023),
+                  ( 0.015, 0.000),(-0.015, 0.000),(-0.005, 0.000),
+                  ( 0.015,-0.023)]
+
+letterVPolygon :: Polygon
+letterVPolygon  = [(-0.015, 0.023),(0.000, -0.023),( 0.015, 0.023)]
+
+letterSpacePolygon :: Polygon
+letterSpacePolygon = []
